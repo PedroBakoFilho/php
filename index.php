e553c8c7fc1f50a56b1ab91b74e22bf25169c207
Pedro Bako Filho <pedrobakofilho@outlook.com>
2023-02-07 18:08:23 -0300

    resolvi o meu erro!

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
  echo "Olá mundo456!";
 
- echo "Olá Udemy!";
-=======
- echo "Olá mundo!123";
-
- echo "Olá Hcode!";
->>>>>>> 4420a25f02b7ea04ab99e623b42e649f1d81be5f
+ echo "Olá Udemy e Hcode!";
 
 ?>
